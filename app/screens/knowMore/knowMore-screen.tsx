0265df27ab8f4a058ad4f8b38033fd11012e7494
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-08-06 18:40:06 +0200

    feat(): new knowmore items and fix gradle warnings

--- app/screens/knowMore/knowMore-screen.tsx
+++ app/screens/knowMore/knowMore-screen.tsx
@@ -1,11 +1,11 @@
-import {NativeStackScreenProps} from '@react-navigation/native-stack';
-import React, {FC} from 'react';
-import {View, Linking} from 'react-native';
-import {NavigatorParamList} from '../../navigators';
-import {observer} from 'mobx-react-lite';
-import {getAppStyle, spacing} from '../../theme';
-import {AppCard, Screen, Text} from '../../components';
-import {TxKeyPath, i18n} from '../../i18n';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { FC } from 'react';
+import { View, Linking } from 'react-native';
+import { NavigatorParamList } from '../../navigators';
+import { observer } from 'mobx-react-lite';
+import { getAppStyle, spacing } from '../../theme';
+import { AppCard, Screen, Text } from '../../components';
+import { TxKeyPath, i18n } from '../../i18n';
 import LinkLogo from '../../../assets/icons/open-outline.svg';
 
 interface itemsKnowMore {
@@ -15,7 +15,7 @@ interface itemsKnowMore {
 
 export const KnowMoreScreen: FC<
   NativeStackScreenProps<NavigatorParamList, 'knowMore'>
-> = observer(({navigation}) => {
+> = observer(({ navigation }) => {
   const appStyle = getAppStyle();
 
   const textStyles = {
@@ -29,48 +29,41 @@ export const KnowMoreScreen: FC<
   const items: itemsKnowMore[] = [
     {
       title: 'knowMoreScreen.subtitle1',
-      url: 'https://igualdadynodiscriminacion.igualdad.gob.es/home.do',
+      url: 'https://riunet.upv.es/collections/911543c6-52b4-4cb8-acca-6b8035e6cf56',
     },
     {
       title: 'knowMoreScreen.subtitle2',
+      url: 'https://podcast.upv.es/programa/tiempo-gitano/',
+    },
+    {
+      title: 'knowMoreScreen.subtitle3',
+      url:
+        i18n.locale === 'ca'
+          ? 'https://www.igualdad.gob.es/wp-content/uploads/Medios-graficos-faldon_Musulmana_246x106_VALEN-scaled.jpg'
+          : 'https://www.igualdad.gob.es/comunicacion/campanas/mirar-con-distinta-cara-es-racismo-021-contra-el-racismo-no-mires-hacia-otro-lado/',
+    },
+    {
+      title: 'knowMoreScreen.subtitle4',
       url:
         i18n.locale === 'ca'
           ? 'https://inclusio.gva.es/va/web/igualdad-diversidad/igualat'
           : 'https://inclusio.gva.es/es/web/igualdad-diversidad/igualat',
     },
     {
-      title: 'knowMoreScreen.subtitle3',
-      url: 'https://www.inclusion.gob.es/oberaxe/es/index.htm',
-    },
-    {
-      title: 'knowMoreScreen.subtitle4',
-      url: 'https://alertcops.ses.mir.es/publico/alertcops/',
-    },
-    {
       title: 'knowMoreScreen.subtitle5',
-      url: 'https://www.coe.int/en/web/european-commission-against-racism-and-intolerance',
+      url: 'https://igualdadynodiscriminacion.igualdad.gob.es/home.do',
     },
     {
       title: 'knowMoreScreen.subtitle6',
-      url: 'https://www.enar-eu.org/',
+      url: 'https://www.inclusion.gob.es/oberaxe/es/index.htm',
     },
     {
       title: 'knowMoreScreen.subtitle7',
-      url: 'https://www.boe.es/buscar/act.php?id=BOE-A-2022-11589',
+      url: 'https://alertcops.ses.mir.es/publico/alertcops/',
     },
     {
       title: 'knowMoreScreen.subtitle8',
-      url:
-        i18n.locale === 'en'
-          ? 'https://www.un.org/en/fight-racism'
-          : 'https://www.un.org/es/fight-racism',
-    },
-    {
-      title: 'knowMoreScreen.subtitle9',
-      url:
-        i18n.locale === 'en'
-          ? 'https://www.un.org/en/hate-speec'
-          : 'https://www.un.org/es/hate-speech',
+      url: 'https://www.boe.es/buscar/act.php?id=BOE-A-2022-11589',
     },
   ];
 
@@ -82,7 +75,8 @@ export const KnowMoreScreen: FC<
     <Screen
       preset="scroll"
       backgroundColor={appStyle.screen.backgroundColor}
-      style={appStyle.screen}>
+      style={appStyle.screen}
+    >
       {items.map(item => {
         return (
           <AppCard
@@ -100,13 +94,14 @@ export const KnowMoreScreen: FC<
                 width: 0,
                 height: 0,
               },
-            }}>
+            }}
+          >
             <LinkLogo
               width={30}
               height={30}
-              style={{marginRight: spacing.medium}}
+              style={{ marginRight: spacing.medium }}
             />
-            <View style={{width: 248}}>
+            <View style={{ width: 248 }}>
               <Text tx={item.title} style={textStyles.title} />
             </View>
           </AppCard>
